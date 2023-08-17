b20397aa2bc6cf526c818596947e576492eaebcc
csimpson1 <csimpson.2@gmail.com>
2023-08-16 22:24:01 -0400

    basic folder structure, homepage skeleton

---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,17 @@
+import './Homepage.css';
+
+const Homepage = () => {
+    return (
+        <div>
+            <div className='homepage-container'>
+                <div>Hello, my name is</div>
+                <div>Colby Simpson</div>
+                <div>and I am a </div>
+                <div>developer</div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Homepage;
