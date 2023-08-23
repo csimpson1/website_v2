6395c3e8b9f71e211df559a8fd3a583bf706ce4f
csimpson1 <csimpson.2@gmail.com>
2023-08-22 20:47:29 -0400

    Added prop for rotation of circle element

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -3,6 +3,7 @@ import BuiltDescription from '../about/built/BuiltDescription';
 import LogoAnimation from './logo-animation/LogoAnimation';
 
 const Homepage = () => {
+    
     return (
         <div>
             <div className='homepage-container'>
@@ -10,7 +11,18 @@ const Homepage = () => {
                 <div>Colby Simpson</div>
                 <div>and I am a </div>
                 <div>developer</div>
-                <BuiltDescription></BuiltDescription>
+                <BuiltDescription/>
+                <div>
+                    <div className="logo-row">
+                    <LogoAnimation angle="75deg"/>
+                    </div>
+                    <div className="logo-row">
+                        <LogoAnimation/>
+                        <LogoAnimation/>
+                        <LogoAnimation/>
+                    </div>
+
+                </div>
                 <LogoAnimation></LogoAnimation>
             </div>
         </div>
--- src/components/homepage/logo-animation/LogoAnimation.js
+++ src/components/homepage/logo-animation/LogoAnimation.js
@@ -1,8 +1,10 @@
 import './LogoAnimation.css'
 
-export const LogoAnimation = () => {
+export const LogoAnimation = ({angle}) => {
+    const xRotation = (angle ?? '0deg');
+
     return (
-        <div className="holder">
+        <div className="holder" style={{transform: `rotateX(${xRotation})`}}>
             <span className="ring-3">
                 <span className="ring-2">
                     <span className="ring-1">
